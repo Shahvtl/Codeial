a7bf65db80240d403dad96c66f63b60e85925aea
shahul <shahulmicchu46@gmail.com>
2022-09-26 14:19:55 +0530

    Post a Post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,21 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie ('user_id', 100);
-    return res.render('home',{
-        title: 'Home'
+const Post = require('../models/post');
+
+    module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie ('user_id', 100);
+
+    //     Post.find({}, function(err, posts){
+    //         return res.render('home', {
+    //             title: "Codeial | Home",
+    //             posts: posts
+    //         });
+    //     });
+
+    //populate the user of each posts
+    Post.find({}).populate('user').exec(function(err, posts){
+    return res.render('home', {
+        title: "Codeial | Home",
+        posts: posts
+    });
     });
 }
